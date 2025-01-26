a2b4d3bbae6f51ca8c39c3a9c2ae3c99dc6e43fc
accion-universitaria <accionuniversitaria2025@gmail.com>
2025-01-26 02:41:35 -0300

    Delete ACCIÓN UNIVERSITARIA directory

--- ACCIÓN UNIVERSITARIA/IMAGENES/archivos-js/paginas.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-   document.querySelector('.dropdown-button').addEventListener('click', function() {
-        const dropdown = document.querySelector('.dropdown');
-        dropdown.classList.toggle('active');
-    });
-});
-
